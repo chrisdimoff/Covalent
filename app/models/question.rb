bbc3ed25e664d22c365dc2e7e8f9d57f1b72bca0
Chris Dimoff <chrisdimoff@gmail.com>
2016-11-28 23:58:55 -0800

    Display mc and checklist questions and options on manager_survey #show view

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,29 @@ class Question < ApplicationRecord
   has_many :options, dependent: :destroy
 
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
+
+  def mc?
+    if self.kind == "mc"
+      return true
+    else
+      return false
+    end
+  end
+
+  def matrix?
+    if self.kind == "matrix"
+      return true
+    else
+      return false
+    end
+  end
+
+  def checklist?
+    if self.kind == "checklist"
+      return true
+    else
+      return false
+    end
+  end
+
 end
