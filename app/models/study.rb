310200a3c71215bdadb972a359d05e0e07dfa8bc
Chris Dimoff <chrisdimoff@gmail.com>
2016-11-30 11:07:55 -0800

    Change studies to have_many manager_surveys and fixed bug on study show for displaying the last manager form when clicking view manager form

--- app/models/study.rb
+++ app/models/study.rb
@@ -12,7 +12,7 @@ class Study < ApplicationRecord
 
   has_many :employees, through: :employee_participations, source: :employee
 
-  has_one :manager_survey, dependent: :destroy
+  has_many :manager_surveys, dependent: :destroy
   has_one :employee_survey, dependent: :destroy
 
   has_many :entries, dependent: :destroy
