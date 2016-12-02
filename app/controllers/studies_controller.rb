fbcdd4edfc7acef7592075bd4c9957d42bd81940
Chris Dimoff <chrisdimoff@gmail.com>
2016-12-02 12:27:10 -0800

    Added manager dashboard widgets

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -11,10 +11,48 @@ class StudiesController < ApplicationController
   def show
     @manager = Manager.last
 
-    @managers_signed_up = @study.managers.count
-    @employees_signed_up = @study.employees.count
-    @managers_not_signed_up = @study.num_of_managers - @managers_signed_up
+    # # logic to count managers for chart
+    # @managers_signed_up_active = 0
+    # @study.managers.each do |mgr|
+    #   num_of_entries = mgr.entries.where(study_id: @study.id).count
+    #   if  num_of_entries > 0
+    #
+    #     @managers_signed_up_active += 1
+    #   end
+    # end
+    # @managers_signed_up = @study.managers.count
+    # @managers_not_signed_up = @study.num_of_managers - @managers_signed_up
+    # @managers_signed_up_inactive = @managers_signed_up - @managers_signed_up_active
+    # # ######
 
+    # logic to count managers for chart
+    @inactive_managers = []
+    @study.managers.each do |mgr|
+      num_of_entries = mgr.entries.where(study_id: @study.id).count
+      if  num_of_entries == 0
+
+        @inactive_managers.push(mgr)
+      end
+    end
+    @managers_signed_up = @study.managers.count
+    @managers_not_signed_up = @study.num_of_managers - @managers_signed_up
+    @managers_signed_up_inactive = @inactive_managers.count
+    @managers_signed_up_active = @managers_signed_up - @inactive_managers.count
+    # ######
+
+    # logic to count employees for chart
+    @employees_signed_up_active = 0
+    @study.employees.each do |emp|
+      num_of_entries = emp.entries.where(study_id: @study.id).count
+      if  num_of_entries > 0
+
+        @employees_signed_up_active += 1
+      end
+    end
+    @employees_signed_up = @study.employees.count
+    @employees_not_signed_up = @study.num_of_employees - @employees_signed_up
+    @employees_signed_up_inactive = @employees_signed_up - @employees_signed_up_active
+    # #####
   end
 
   def create
