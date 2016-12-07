6d654662bf2bc83b5b0e882d7c749447c9faad27
Chris Dimoff <chrisdimoff@gmail.com>
2016-12-07 10:23:03 -0800

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,8 @@ Rails.application.configure do
 
   config.assets.compile = true
   # config.assets.paths << "#{Rails}/vendor/assets/fonts"
-  # config.serve_static_assets = true
-  # config.assets.digest = true
+  config.serve_static_assets = true
+  config.assets.digest = true
 
 
   # Code is not reloaded between requests.
@@ -32,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
