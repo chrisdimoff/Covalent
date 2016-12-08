b33e419ca88c70fac996652c3a44cc44754dbf2f
Chris Dimoff <chrisdimoff@gmail.com>
2016-12-08 09:09:46 -0800

    changed unbounce name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def unbounce
 
       if Rails.env =="production"
-        redirect_to "http://unbouncepages.com/mhapp-home/"
+        redirect_to "http://unbouncepages.com/covalent-beta/"
       else
         redirect_to root_path
       end
